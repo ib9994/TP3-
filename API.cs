e31c6ef13cad2778b5881b6f8ae199eec2234fdb
ib9994 <63601558+ib9994@users.noreply.github.com>
2021-04-08 16:28:50 +0200

    Add files via upload

---
+++ API.cs
@@ -0,0 +1,272 @@
+﻿using TradingBot.Info;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Timers; 
+using System.ComponentModel;
+
+namespace TradingBot
+{
+	public class API
+	{
+		/// <summary>
+		/// Le robot prend une paire de devises, vérifie les transactions précédentes pour cette paire.
+		/// Seul le contraire de la dernière transaction peut être exécuté (technique de maximisation de la quantité de stock).
+		/// Si le prix a changé selon le pourcentage souhaité, une transaction est exécutée. 
+		/// </summary>
+
+		public static string Url { get; set; } = "https://www.binance.com/api/";
+
+		/// <summary>
+		/// Tester la connectivité à l'API Rest de Binance
+		/// </summary>
+		/// <returns>Valeur booléenne (vrai/faux) basée sur le succès.</returns>
+		public static bool Ping()
+		{
+			string apiRequestUrl = $"{Url}v1/ping";
+
+			string response = Server.webRequest(apiRequestUrl, "GET", null);
+			if (response == "{}")
+				return true;
+			else
+				return false;
+		}
+
+		/// <summary>
+		/// Tester la connectivité à l'API Rest de Binance et obtenir l'heure actuelle du serveur.
+		/// </summary>
+		/// <returns>Chaîne de l'heure actuelle en millisecondes</returns>
+		public static string Time()
+		{
+			string apiRequestUrl = $"{Url}v1/time";
+
+			var response = Server.webRequest(apiRequestUrl, "GET", null);
+			return response;
+		}
+
+
+		/// <summary>
+		/// Obtenir les prix de tous les symboles
+		/// </summary>
+		/// <returns>Prix actuels de tous les symbols</returns>
+		public static SymbolPrice[] GetAllPrices()
+		{
+			string apiRequestUrl = $"{Url}v1/ticker/allPrices";
+
+			var response = Server.webRequest(apiRequestUrl, "GET", null);
+			var parsedResponse = JsonConvert.DeserializeObject<SymbolPrice[]>(response);
+
+			return parsedResponse;
+		}
+
+
+		/// <summary>
+		/// Obtenir le prix actuel pour le symbole donné
+		/// </summary>
+		/// <param name="symbol">Asset symbol e.g.ETHBTC</param>
+		/// <returns>The price. Double.</returns>
+		public static double GetOnePrice(string symbol)
+		{
+			var symbolPrices = GetAllPrices();
+
+			SymbolPrice symbolPrice = (from sym in symbolPrices where sym.Symbol == symbol select sym).FirstOrDefault();
+			if (symbolPrice == null)
+			{
+				throw new ApplicationException($"No symbol, {symbol}, exists");
+			}
+
+			return symbolPrice.Price;
+		}
+
+
+		/// <summary>
+		/// Obtenir des informations sur le compte, y compris tous les soldes d'actifs
+		/// </summary>
+		/// <param name="recvWindow">Intervalle (en millisecondes) dans lequel la demande doit être traitée dans un certain nombre de millisecondes ou être rejetée par le serveur. Valeur par défaut : 5000 millisecondes</param>
+		/// <returns>Objet AccountInformation comprenant les soldes d'actifs actuels</returns>
+		public static AccountInformation GetAccountInformation()
+		{
+			string apiRequestUrl = $"{Url}v3/account";
+
+			string query = $"";
+			query = $"{query}&timestamp={Server.getTimeStamp()}";
+
+			var signature = Server.getSignature(General.SecretKey, query);
+			query += "&signature=" + signature;
+
+			apiRequestUrl += "?" + query;
+
+			var response = Server.webRequest(apiRequestUrl, "GET", General.ApiKey);
+
+			var parsedResponse = JsonConvert.DeserializeObject<AccountInformation>(response);
+			return parsedResponse;
+		}
+
+		/// <summary>
+		/// Obtenez vos ordres ouverts pour le symbole donné
+		/// </summary>
+		/// <param name="symbol">Asset symbol e.g.ETHBTC</param>
+		/// <returns>Une liste d'objets Order avec les données de l'ordre</returns>
+		public static Order[] GetOpenOrders(string symbol)
+		{
+			string apiRequestUrl = $"{Url}v3/openOrders";
+
+			string query = $"symbol={symbol}";
+
+			query = $"{query}&timestamp={Server.getTimeStamp()}";
+
+			var signature = Server.getSignature(General.SecretKey, query);
+
+			query += "&signature=" + signature;
+
+			apiRequestUrl += "?" + query;
+
+			var response = Server.webRequest(apiRequestUrl, "GET", General.ApiKey);
+			var parsedResponse = JsonConvert.DeserializeObject<Order[]>(response);
+			return parsedResponse;
+		}
+
+		/// <summary>
+		/// Obtenir des transactions pour un compte et un symbole spécifiques
+		/// </summary>
+		/// <param name="symbol">Asset symbol e.g.ETHBTC</param>
+		/// <returns>Une liste de Trades</returns>
+		public static Trades[] GetMyTrades(string symbol)
+		{
+			string apiRequestUrl = $"{Url}v3/myTrades";
+
+			string query = $"symbol={symbol}";
+
+			query = $"{query}&timestamp={Server.getTimeStamp()}";
+
+			var signature = Server.getSignature(General.SecretKey, query);
+			query += "&signature=" + signature;
+
+			apiRequestUrl += "?" + query;
+
+			var response = Server.webRequest(apiRequestUrl, "GET", General.ApiKey);
+			var parsedResponse = JsonConvert.DeserializeObject<Trades[]>(response);
+			return parsedResponse;
+		}
+
+		/// <summary>
+		/// Obtenir le dernier Trade
+		/// </summary>
+		/// <returns>La dernière transaction. Objet Trades</returns>
+		/// <param name="symbol">Symbol.</param>
+		public static Trades GetLastTrade(string symbol)
+		{
+			var parsedResponse = GetMyTrades(symbol);
+
+			if (parsedResponse.Length != 0)
+				return parsedResponse[parsedResponse.Length - 1];
+			else
+				return null;
+		}
+
+		/// <summary>
+		/// Place un ordre
+		/// </summary>
+		/// <returns>L'objet de l'ordre</returns>
+		/// <param name="symbol">Symbole de la monnaie tradé, eg BCCETH</param>
+		/// <param name="side">Sens de l'ordre , acheter ou vendre</param>
+		/// <param name="type">Type de commande, voir Set.OrderTypes </param>
+		/// <param name="timeInForce">La durée de la commande sera active pour</param>
+		/// <param name="quantity">Montant à trader</param>
+		/// <param name="price">Prix auquel acheter</param>
+
+		public static Order PlaceOrder(string symbol, OrderSides side, OrderTypes type, TimesInForce timeInForce, double quantity, double price)
+		{
+			string apiRequestUrl = "";
+
+			if (General.testCase == true)
+				apiRequestUrl = $"{Url}v3/order/test";
+			else
+				apiRequestUrl = $"{Url}v3/order";
+
+
+			string query = $"symbol={symbol}&side={side}&type={type}&timeInForce={timeInForce}&quantity={quantity}&price={price}";
+
+			query = $"{query}&timestamp={Server.getTimeStamp()}";
+
+			var signature = Server.getSignature(General.SecretKey, query);
+			query += "&signature=" + signature;
+
+			apiRequestUrl += "?" + query;
+			var response = Server.webRequest(apiRequestUrl, "POST", General.ApiKey);
+
+			var parsedResponse = JsonConvert.DeserializeObject<Order>(response);
+			return parsedResponse;
+		}
+
+		/// <summary>
+		/// Place un ordre au marché. (Ordre au prix actuel du marché, ne nécessite pas de paramètres de prix ou de timeInForce).
+		/// </summary>
+		/// <returns>L'objet de l'ordre</returns>
+		/// <param name="symbol">Symbole de la monnaie a trader, eg BCCETH.</param>
+		/// <param name="side">Sens de l'odre , acheter ou vendre.</param>
+		/// <param name="quantity">Montant à trader.</param>
+
+		public static Order PlaceMarketOrder(string symbol, OrderSides side, double quantity)
+		{
+			string apiRequestUrl = ""; 
+
+			if (General.testCase == true)
+				apiRequestUrl = $"{Url}v3/order/test";
+			else
+				apiRequestUrl = $"{Url}v3/order";
+
+			string query = $"symbol={symbol}&side={side}&type={OrderTypes.MARKET}&quantity={quantity}";
+			query = $"{query}&timestamp={Server.getTimeStamp()}";
+
+			var signature = Server.getSignature(General.SecretKey, query);
+			query += "&signature=" + signature;
+
+			apiRequestUrl += "?" + query;
+			var response = Server.webRequest(apiRequestUrl, "POST", General.ApiKey);
+
+			var parsedResponse = JsonConvert.DeserializeObject<Order>(response);
+			return parsedResponse;
+		}
+
+
+		/***********
+
+		//Ping
+		var pingTest = Ping();
+
+		//Temps
+		var timeTest = Time();
+
+		///Retrouver vos informations de compte
+		//Retourné sous forme d'objet, voir Set.cs pour l'analyser
+		var accountInfo = GetAccountInformation();
+
+		///Retrouver les prix de toutes les paires de devises disponibles
+		//Retourné sous forme de liste d'objets, voir Set.cs ou la fonction GetOnePrice() pour l'analyser
+		var allPrices = GetAllPrices();
+
+		///Retrouver les prix d'une paire de devises/symbole spécifique
+		var onePrice = GetOnePrice("BCCETH");
+
+		///Retrouver tous les ordres ouverts sur votre compte
+		var openOrders = GetOpenOrders("BCCETH");
+
+		///Obtenez l'historique de vos transactions liées à une paire de devises/un symbole spécifique.
+		//Retourné sous la forme d'une liste d'objets, voir Set.cs ou la fonction GetLastTrade() pour l'analyser.
+		var trades = GetMyTrades("XRPETH");
+
+		///Obtenez votre dernière transaction pour une paire de devises/un symbole spécifique.
+		//Retourné sous forme d'objet, voir Set.cs pour l'analyser
+		var lastTrade = GetLastTrade("XRPETH");
+
+		///Placez n'importe quel type d'ordre accepté, défini en phase "test" car non utilisé.
+		//var order = PlaceOrder("BCCETH", OrderSides.SELL, OrderTypes.MARKET, TimesInForce.GTC, 0.01, 2.09) ;
+
+		///Placer un ordre de marché
+		var marketOrder = PlaceMarketOrder("BCCETH", OrderSides.SELL, 0.01);
+
+		**********/
+
+	}
+}
---
+++ AlgoTrading.cs
@@ -0,0 +1,100 @@
+﻿using TradingBot.Info;
+using System;
+using System.Linq;
+
+
+
+namespace TradingBot
+{
+	public class AlgoTrading
+	{
+
+
+		public static void Bot()
+		{
+			string symbol = General.C1 + General.C2;
+
+			//Obtenir les informations de compte
+			var accInfo = API.GetAccountInformation();
+
+			//Vérifier les fonds du compte pour les deux monnaies
+			AssetBalance Currency1 = (from coin in accInfo.Balances where coin.Asset == General.C1 select coin).FirstOrDefault();
+			var freeCurrency1 = Currency1.Free;
+
+			AssetBalance Currency2 = (from coin in accInfo.Balances where coin.Asset == General.C2 select coin).FirstOrDefault();
+			var freeCurrency2 = Currency2.Free;
+
+			//Obtenir le prix du dernier trade BCCBTC
+			var lastTrade = API.GetLastTrade(symbol);
+			var lastTradePrice = 0.0;
+			var lastTradeSide = OrderSides.SELL; //si lastTrade == null, cela signifie acheter la Devise1
+
+
+			//obtenir le prix et l'OrderSide de la dernière transaction (le cas échéant)
+			if (lastTrade != null)
+			{
+				lastTradePrice = lastTrade.Price;
+
+				if (lastTrade.isBuyer == true)
+					lastTradeSide = OrderSides.BUY;
+				else
+					lastTradeSide = OrderSides.SELL;
+			}
+
+			//Regarder le prix actuel
+			var currentPrice = API.GetOnePrice(symbol);
+
+			//Calculer le pourcentage de changement de prix réel
+			var priceChange = 100 * (currentPrice - lastTradePrice) / currentPrice;
+
+			Console.WriteLine("Current Price is " + currentPrice);
+			Console.WriteLine("Price Change is " + priceChange);
+
+			//Creer un ordre
+			Order marketOrder = null;
+
+			if (lastTradeSide == OrderSides.BUY && priceChange > General.percentageChange)
+			{
+				//si la dernière commande était un achat et que le prix a augmenté.
+				//Vendre C1
+				marketOrder = API.PlaceMarketOrder(symbol, OrderSides.SELL, General.quatityPerTrade);
+
+			}
+			else if (lastTradeSide == OrderSides.SELL && priceChange < -General.percentageChange)
+			{
+				//si le dernier ordre était une vente, et que le prix a baissé.
+				//Acheter C1
+				marketOrder = API.PlaceMarketOrder(symbol, OrderSides.BUY, General.quatityPerTrade);
+			}
+
+
+			//Déclarations
+			if (marketOrder == null)
+			{
+				Console.WriteLine("No trade was made.");
+				var actualLastTrade = API.GetLastTrade(symbol);
+				if (actualLastTrade.isBuyer == true)
+				{
+					Console.WriteLine(actualLastTrade.Qty + " of " + General.C1 + " was previously bought for " + actualLastTrade.Price);
+				}
+				else if (actualLastTrade.isBuyer == false)
+				{
+					Console.WriteLine(actualLastTrade.Qty + " of " + General.C1 + " was previously sold for " + actualLastTrade.Price);
+				}
+			}
+			else
+			{
+				var newLastTrade = API.GetLastTrade(symbol);
+				if (marketOrder.Side == OrderSides.BUY)
+				{
+					Console.WriteLine(newLastTrade.Qty + " of " + General.C1 + " was bought for " + newLastTrade.Price);
+				}
+				else if (marketOrder.Side == OrderSides.SELL)
+				{
+					Console.WriteLine(newLastTrade.Qty + " of " + General.C1 + " was sold for " + newLastTrade.Price);
+				}
+			}
+
+		}
+	}
+}
